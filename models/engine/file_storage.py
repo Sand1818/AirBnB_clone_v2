934209df61a3f8aa82e112492bdc70af1e1c613a
Sand1818 <sandiso.jele@gmail.com>
2024-04-17 23:15:00 +0200

    def all update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        sj = {}
+        for a, b in self.__objects.items():
+            if b.__class__ == cls:
+                sj[a] = b
+            return sj
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
